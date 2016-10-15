6c6c1734bc9e2c05749e28806a73fb779a9194ca
DrakeW <junyuw@berkeley.edu>
2016-10-14 17:33:01 -0700

    added function to generate scatterplot of regression

--- code/scripts/regression-script.R
+++ code/scripts/regression-script.R
@@ -17,4 +17,21 @@ for (i in 1:3) {
   dev.off()
 }
 
+scatterplot_generator <- function (data, col_name1, col_name2) {
+  image_file <- paste("images/scatterplot-", col_name1, "-", col_name2, ".png", sep = "")
+  png(image_file)
+  plot(data[,col_name1], 
+       data[,col_name2], 
+       main = paste("Scatterplot of", col_name1, " and", col_name2),
+       xlab = col_name1, 
+       ylab = col_name2)
+  reg <- lm(data[,col_name2] ~ data[,col_name1])
+  abline(reg, col = "blue")
+  dev.off()
+}
+
+cols <- c("TV", "Newspaper", "Radio")
+for (col in cols) {
+  scatterplot_generator(csv_data, col, "Sales")
+}
 
